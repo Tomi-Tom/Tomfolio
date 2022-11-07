82624312de2506bff2df006a46400f8e6e9faa78
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2022-11-07 03:56:51 +0100

    add: all base for my website

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function test() {
+    return (
+        <div>
+            <Head>
+                <title>Tom Test</title>
+            </Head>
+            <h2>My Big Test</h2>
+        </div>
+    )
+}
