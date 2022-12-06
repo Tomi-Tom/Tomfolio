33eb0ef4873f62538e4b007ef07c2d8f3807f481
Tomi-Tom <91875715+Tomi-Tom@users.noreply.github.com>
2022-12-06 15:09:53 +0100

    Update and rename topbar.tsx to Navbar.tsx

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Accueil</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>À propos</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
