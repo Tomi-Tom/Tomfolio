c4739f61bf6a5dffd8bbc58d387d0014b096ab4a
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-02-08 16:50:00 +0100

    fix: def var in css file

--- src/components/cube.tsx
+++ src/components/cube.tsx
@@ -1,12 +1,12 @@
 export default function Cube() {
     return (
-        <div className="cube" style={{ "--s": `200`}}>
+        <div className="cube" style={{ "--size": "400px" }}>
             <div className="top"></div>
             <div>
-                <span style={{ "--i": `0` }}></span>
-                <span style={{ "--i": `1` }}></span>
-                <span style={{ "--i": `2` }}></span>
-                <span style={{ "--i": `3` }}></span>
+                <span style={{ "--index": 0 }}></span>
+                <span style={{ "--index": 1 }}></span>
+                <span style={{ "--index": 2 }}></span>
+                <span style={{ "--index": 3 }}></span>
             </div>
         </div>
     );
