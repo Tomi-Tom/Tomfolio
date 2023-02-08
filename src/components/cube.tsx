da406d2f3962ca78ce6689c0162b71965004b184
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2023-02-08 16:39:07 +0100

    fix: try something to fix type issue

--- src/components/cube.tsx
+++ src/components/cube.tsx
@@ -1,12 +1,12 @@
 export default function Cube() {
     return (
-        <div className="cube" style={{ "--s": "400px" }}>
+        <div className="cube" style={{ "--s": `200`}}>
             <div className="top"></div>
             <div>
-                <span style={{ "--i": 0 }}></span>
-                <span style={{ "--i": 1 }}></span>
-                <span style={{ "--i": 2 }}></span>
-                <span style={{ "--i": 3 }}></span>
+                <span style={{ "--i": `0` }}></span>
+                <span style={{ "--i": `1` }}></span>
+                <span style={{ "--i": `2` }}></span>
+                <span style={{ "--i": `3` }}></span>
             </div>
         </div>
     );
