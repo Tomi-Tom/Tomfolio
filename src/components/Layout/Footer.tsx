b5b6fb74dbb899863ce28a435b062026350417b5
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-04-21 01:25:21 +0200

    fix: temp fix for footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,29 +1,41 @@
 import type { ReactElement } from 'react'
-
+import logo from '../../assets/logo.png'
 export default function Footer(): ReactElement {
+  const links = [
+    {
+      href: 'https://www.instagram.com/tombp_dev/',
+      icon: 'https://cdn-icons-png.flaticon.com/512/174/174855.png',
+      alt: 'Instagram',
+    },
+    {
+      href: 'https://www.linkedin.com/in/tom-bp/',
+      icon: 'https://cdn-icons-png.flaticon.com/256/174/174857.png',
+      alt: 'LinkedIn',
+    },
+    {
+      href: 'https://github.com/Tomi-Tom',
+      icon: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+      alt: 'GitHub',
+    },
+  ]
   return (
-    <div className="bottom-0 left-0 w-full h-16 bg-background-2 text-white flex items-center justify-center border-t border-border-1">
-      <div className="flex flex-row space-x-4 max-w-screen-xl mx-auto justify-between">
-        <a
-          href="https://www.linkedin.com/in/tom-bp/"
-          className="hover:text-interactive-3"
-        >
-          <img
-            src="https://img.icons8.com/ios/50/000000/linkedin.png"
-            alt="LinkedIn"
-            className="w-8 h-8"
-          />
+    <div className="bottom-0 left-0 h-16 bg-background-2 text-white flex items-center justify-center border-t border-border-1">
+      <div className="flex flex-row  justify-between items-center px-20 space-x-96">
+        <a href="/" className="flex items-center">
+          <img src={logo} alt="logo" className=" w-16 h-16 hover:scale-105" />
         </a>
-        <a
-          href="https://www.linkedin.com/in/tom-bp/"
-          className="hover:text-interactive-3"
-        >
-          <img
-            src="https://img.icons8.com/ios/50/000000/github.png"
-            alt="GitHub"
-            className="w-8 h-8"
-          />
-        </a>
+        <p className={'px-60'}>© 2021 Tomi Tom</p>
+        <div className="flex flex-row space-x-4">
+          {links.map((link) => (
+            <a
+              href={link.href}
+              className="hover:text-interactive-3"
+              key={link.alt}
+            >
+              <img src={link.icon} alt={link.alt} className="w-8 h-8" />
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   )
