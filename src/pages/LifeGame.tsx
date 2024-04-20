77a1f2c45e996de9ac8d571aff71ea52748c9b71
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-04-20 22:49:21 +0200

    feat: add cells selection

--- src/pages/LifeGame.tsx
+++ src/pages/LifeGame.tsx
@@ -3,15 +3,36 @@
 import { ReactElement, useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 
+const initialCells = new Array(100)
+  .fill(null)
+  .map(() => new Array(100).fill(false))
+
 export default function LifeGamePage(): ReactElement {
   const [border, setBorder] = useState(10)
+  const [cells, setCells] = useState<Array<Array<boolean>>>(initialCells)
 
   const handleDecrease = () => {
     setBorder(border - 1)
+    setCells(
+      new Array((border - 1) ** 2)
+        .fill(null)
+        .map(() => new Array((border - 1) ** 2).fill(false))
+    )
   }
 
   const handleIncrease = () => {
     setBorder(border + 1)
+    setCells(
+      new Array((border + 1) ** 2)
+        .fill(null)
+        .map(() => new Array((border + 1) ** 2).fill(false))
+    )
+  }
+
+  const handleCellClick = (i: number) => {
+    let newCells = [...cells]
+    newCells[i] = !newCells[i]
+    setCells(newCells)
   }
 
   return (
@@ -24,14 +45,15 @@ export default function LifeGamePage(): ReactElement {
             gridTemplateRows: `repeat(${border}, 1fr)`,
           }}
         >
-          {Array.from({ length: border * border }, (_, i) => (
+          {cells.map((state, i) => (
             <div
               key={i}
-              className={`bg-gray-200 border border-gray-300`}
+              className={`${state ? 'bg-background-2' : 'bg-gray-300'}`}
               style={{
                 width: (1000 - 4 * border) / border,
                 height: (1000 - 4 * border) / border,
               }}
+              onClick={() => handleCellClick(i)}
             />
           ))}
         </div>
