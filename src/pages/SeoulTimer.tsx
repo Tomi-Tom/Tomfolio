b91e9aacf3e1a843c6f9802416ec789be6aad8a2
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-06 22:49:15 +0200

    add first squad

--- src/pages/SeoulTimer.tsx
+++ src/pages/SeoulTimer.tsx
@@ -14,13 +14,18 @@ type particule = {
 
 export default function SeoulTimerPage(): ReactElement {
   const departure = new Date('2024-08-22T21:00:00')
+  const departure2 = new Date('2024-07-25T12:00:00')
   const [timeLeft, setTimeLeft] = useState(
     departure.getTime() - new Date().getTime()
+  )
+  const [timeLeft2, setTimeLeft2] = useState(
+    departure2.getTime() - new Date().getTime()
   )
   const [particles, setParticles] = useState<particule[]>([])
 
   setInterval(() => {
     setTimeLeft(departure.getTime() - new Date().getTime())
+    setTimeLeft2(departure2.getTime() - new Date().getTime())
   }, 1000)
 
   useEffect(() => {
@@ -54,7 +59,7 @@ export default function SeoulTimerPage(): ReactElement {
           className="h-full w-full object-cover max-lg:hidden"
         />
       </div>
-      <div className="min-w-screen relative flex min-h-screen items-center justify-center pb-64">
+      <div className="min-w-screen relative flex min-h-screen flex-col items-center justify-center pb-64">
         <div className="z-10 flex size-full items-center justify-center px-48 pb-24 pt-36 max-lg:px-8">
           <div
             className="size-full transform cursor-pointer select-none items-center justify-center rounded-2xl  bg-black bg-opacity-15 p-8 text-center shadow-2xl transition duration-500 hover:translate-y-8 active:scale-105"
@@ -94,6 +99,51 @@ export default function SeoulTimerPage(): ReactElement {
                 {Math.floor((timeLeft / (1000 * 60)) % 60)}:
                 {Math.floor((timeLeft / 1000) % 60) >= 10 ? '' : '0'}
                 {Math.floor((timeLeft / 1000) % 60)}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="z-10 flex size-full items-center justify-center px-96 pb-24 pt-36 max-lg:px-8">
+          <div
+            className="size-full transform cursor-pointer select-none items-center justify-center rounded-2xl  bg-black bg-opacity-15 p-8 text-center shadow-2xl transition duration-500 hover:translate-y-8 active:scale-105"
+            onClick={() => {
+              setParticles([
+                ...particles,
+                {
+                  x: Math.random() * 1000,
+                  y: Math.random() * 1000,
+                  size: 30 + Math.random() * 50,
+                  speed: Math.random() * 10,
+                  img: krFlag,
+                },
+              ])
+            }}
+          >
+            <img
+              src={krFlag}
+              alt="Korea Flag"
+              className="sm-hidden h-full w-full rounded-2xl object-cover pb-4 shadow-2xl sm:hidden"
+            />
+            <h1 className="text-4xl font-bold">Seoul Timer</h1>
+            <p className="p-2 text-2xl font-light">
+              Time left until departure for the first squad
+            </p>
+            <div className="flex w-full flex-row items-center justify-center py-8 max-lg:flex-col">
+              <p className="px-8 text-6xl font-extrabold max-lg:text-4xl">
+                {Math.floor(timeLeft2 / (1000 * 60 * 60 * 24))} Days
+              </p>
+              <p className="px-8 text-6xl font-extrabold max-lg:hidden">
+                {' |'}
+              </p>
+              <p className="px-8 text-6xl font-extrabold max-lg:text-4xl">
+                {Math.floor((timeLeft2 / (1000 * 60 * 60)) % 24) >= 10
+                  ? ''
+                  : '0'}
+                {Math.floor((timeLeft2 / (1000 * 60 * 60)) % 24)}:
+                {Math.floor((timeLeft2 / (1000 * 60)) % 60) >= 10 ? '' : '0'}
+                {Math.floor((timeLeft2 / (1000 * 60)) % 60)}:
+                {Math.floor((timeLeft2 / 1000) % 60) >= 10 ? '' : '0'}
+                {Math.floor((timeLeft2 / 1000) % 60)}
               </p>
             </div>
           </div>
