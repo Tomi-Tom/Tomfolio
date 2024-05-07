3d340cffce87be50d44069a6c953c7c7b2039f75
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-05-07 17:08:05 +0200

    style: add some style to card

--- src/components/Home/PreviewProjectsSection.tsx
+++ src/components/Home/PreviewProjectsSection.tsx
@@ -78,9 +78,9 @@ import React from 'react'
 const Card = ({ title, description, image, link, github }) => {
   return (
     <div className="group relative h-56 min-w-64">
-      <div className="size-full rounded-xl border border-border-1 bg-interactive-1 transition-all duration-300 group-hover:-translate-y-5 group-hover:scale-110 group-hover:bg-interactive-2 group-hover:shadow-lg">
+      <div className="size-full rounded-xl border border-border-1 bg-solid-1 transition-all duration-300 group-hover:-translate-y-5 group-hover:scale-110 group-hover:bg-interactive-2 group-hover:shadow-lg">
         <div
-          className="relative h-3/5 w-full cursor-pointer overflow-hidden rounded-t-xl bg-background-1"
+          className="group/img relative h-3/5 w-full cursor-pointer overflow-hidden rounded-t-xl bg-background-1"
           onClick={(): void => {
             window.location.href = '/projects'
           }}
@@ -88,38 +88,43 @@ const Card = ({ title, description, image, link, github }) => {
           <img
             src={image}
             alt={title}
-            className="size-full object-cover transition-all duration-300 hover:opacity-75 group-hover:scale-110"
+            className="over:opacity-75 group/img-hover:scale-110 size-full object-cover transition-all duration-300 group-hover/img:opacity-35"
           />
+          <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-2xl font-bold text-text-2 opacity-0 transition-all duration-300 group-hover/img:opacity-100">
+            {title}
+          </span>
         </div>
-        <div className="p-2 transition-all duration-300 ">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <p className="text-sm transition-all group-hover:opacity-20">
-            {description}
-          </p>
-        </div>
-        <div className="absolute bottom-0 left-1/4 flex justify-center gap-4 rounded-b-xl p-2 opacity-0 transition-all duration-300 group-hover:-translate-y-2 group-hover:opacity-100">
-          {link ? (
+        <div className="group/base w-full">
+          <div className="p-2 transition-all duration-300 ">
+            <h2 className="text-xl font-bold">{title}</h2>
+            <p className="text-sm transition-all duration-300 group-hover/base:opacity-0">
+              {description}
+            </p>
+          </div>
+          <div className="absolute bottom-0 left-1/4 flex justify-center gap-4 rounded-b-xl p-2 opacity-0 transition-all duration-300 group-hover/base:-translate-y-2 group-hover/base:opacity-100">
+            {link ? (
+              <button
+                className="dui-button dui-button-primary border-primary rounded-xl bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50 hover:underline"
+                onClick={(): void => {
+                  window.location.href = link
+                }}
+              >
+                Visit
+              </button>
+            ) : (
+              <div className="dui-button dui-button-primary border-primary select-none rounded-xl bg-gray-50 px-2 py-1 text-background-2 opacity-35">
+                Visit
+              </div>
+            )}
             <button
-              className="dui-button dui-button-primary border-primary rounded-xl border bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50 hover:underline"
+              className="dui-button dui-button-primary border-primary group-hover:bg-primary rounded-xl bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50"
               onClick={(): void => {
-                window.location.href = link
+                window.location.href = github
               }}
             >
-              Visit
+              Github
             </button>
-          ) : (
-            <div className="dui-button dui-button-primary border-primary select-none rounded-xl border bg-gray-50 px-2 py-1 text-background-2 opacity-35">
-              Visit
-            </div>
-          )}
-          <button
-            className="dui-button dui-button-primary border-primary group-hover:bg-primary rounded-xl border bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50"
-            onClick={(): void => {
-              window.location.href = github
-            }}
-          >
-            Github
-          </button>
+          </div>
         </div>
       </div>
     </div>
