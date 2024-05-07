49f8fd49f59821187fb8056a442a2427f2e82389
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-05-07 17:30:28 +0200

    fix

--- src/components/Home/PreviewProjectsSection.tsx
+++ src/components/Home/PreviewProjectsSection.tsx
@@ -43,7 +43,7 @@ export default function PresentationSection(): ReactElement {
   ]
 
   return (
-    <div className="relative flex h-screen flex-col items-center justify-center p-8 sm:p-32">
+    <div className="relative flex h-auto flex-col items-center justify-center p-8 sm:p-32">
       <div className="relative flex h-full w-full flex-col items-center justify-center space-y-4 rounded-2xl bg-background-2 p-8 shadow-lg sm:p-32">
         <h1 className="text-3xl font-bold">Here are some of my projects</h1>
         <p className="text-xl font-light">
@@ -51,7 +51,7 @@ export default function PresentationSection(): ReactElement {
         </p>
         <div
           className={
-            'grid justify-between gap-16 max-lg:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
+            'grid justify-between gap-16 py-8 max-lg:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'
           }
         >
           {projects.map((project, index) => (
@@ -77,8 +77,8 @@ import React from 'react'
 
 const Card = ({ title, description, image, link, github }) => {
   return (
-    <div className="group relative h-56 min-w-64">
-      <div className="size-full rounded-xl border border-border-1 bg-solid-1 transition-all duration-300 group-hover:-translate-y-5 group-hover:scale-110 group-hover:bg-interactive-2 group-hover:shadow-lg">
+    <div className="group relative h-64 min-w-64">
+      <div className="size-full rounded-xl border-2 border-border-1 bg-background-1 transition-all duration-300 group-hover:-translate-y-5 group-hover:scale-110 group-hover:bg-interactive-2 group-hover:shadow-lg">
         <div
           className="group/img relative h-3/5 w-full cursor-pointer overflow-hidden rounded-t-xl bg-background-1"
           onClick={(): void => {
@@ -101,7 +101,7 @@ const Card = ({ title, description, image, link, github }) => {
               {description}
             </p>
           </div>
-          <div className="absolute bottom-0 left-1/4 flex justify-center gap-4 rounded-b-xl p-2 opacity-0 transition-all duration-300 group-hover/base:-translate-y-2 group-hover/base:opacity-100">
+          <div className="absolute bottom-0 left-1/4 flex justify-center gap-4 rounded-b-xl p-4 opacity-0 transition-all duration-300 group-hover/base:-translate-y-2 group-hover/base:opacity-100">
             {link ? (
               <button
                 className="dui-button dui-button-primary border-primary rounded-xl bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50 hover:underline"
