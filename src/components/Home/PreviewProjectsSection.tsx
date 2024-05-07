8d0dc20b5d458c53186b1bc0d4b122ea5d98704b
Tom BARITEAU-PETER <tom.bariteau-peter@epitech.eu>
2024-05-07 15:37:04 +0200

    feat: add some projects i worked on

--- src/components/Home/PreviewProjectsSection.tsx
+++ src/components/Home/PreviewProjectsSection.tsx
@@ -1,34 +1,36 @@
 import type { ReactElement } from 'react'
+import areaLogo from '../../assets/areaLogo.png'
 
 export default function PresentationSection(): ReactElement {
   const projects = [
     {
-      title: 'Project 1',
-      description: 'This is a project I worked on',
-      image: 'https://via.placeholder.com/150',
-      link: 'https://www.google.com',
-      github: 'https://www.github.com',
+      title: 'TomFolio',
+      description: 'This is the currently website you are on, my portfolio',
+      image: 'https://www.tombp.fr/assets/logo-Pce1k1VJ.png',
+      link: 'https://www.tombp.fr',
+      github: 'https://github.com/Tomi-Tom/Tomfolio',
     },
     {
-      title: 'Project 2',
-      description: 'This is another project I worked on',
-      image: 'https://via.placeholder.com/150',
-      link: 'https://www.google.com',
-      github: 'https://www.github.com',
+      title: 'Bedrock',
+      description: 'A blockchain based, note taking app',
+      image: 'https://bedrock-im.crooser.xyz/logo-viol.png',
+      link: 'https://bedrock-im.crooser.xyz/',
+      github: 'https://github.com/EdenComp/Bedrock.im ',
     },
     {
-      title: 'Project 3',
-      description: 'This is a project I worked on',
-      image: 'https://via.placeholder.com/150',
-      link: 'https://www.google.com',
-      github: 'https://www.github.com',
+      title: 'Area',
+      description: 'An app to automate tasks and link services',
+      image: areaLogo,
+      link: null,
+      github: 'https://github.com/RezaRahemtola/Area  ',
     },
     {
-      title: 'Project 4',
-      description: 'This is another project I worked on',
-      image: 'https://via.placeholder.com/150',
-      link: 'https://www.google.com',
-      github: 'https://www.github.com',
+      title: 'Dante Star',
+      description: 'A maze generator and solver in C',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/7/78/Traquair_House_Maze.jpg',
+      link: null,
+      github: 'https://github.com/Tomi-Tom/Dante-Star',
     },
     {
       title: 'Project 5',
@@ -91,16 +93,53 @@ export default function PresentationSection(): ReactElement {
   )
 }
 
+import React from 'react'
+
 const Card = ({ title, description, image, link, github }) => {
   return (
     <div className="group h-56 min-w-64">
       <div className="size-full rounded-xl border border-border-1 bg-gray-500 text-center transition-all duration-300 group-hover:-translate-y-5 group-hover:scale-110 group-hover:shadow-lg">
-        <div className="relative h-2/3 w-full overflow-hidden rounded-t-xl bg-gray-50">
-          <img src={image} alt={title} className="h-full w-full object-cover" />
+        <div
+          className="relative h-3/5 w-full cursor-pointer overflow-hidden rounded-t-xl bg-background-1"
+          onClick={(): void => {
+            window.location.href = '/projects'
+          }}
+        >
+          <img
+            src={image}
+            alt={title}
+            className="size-full object-cover transition-all duration-300 hover:opacity-75 group-hover:scale-110"
+          />
         </div>
-        <div className="p-4">
+        <div className="p-2 transition-all duration-300 ">
           <h2 className="text-xl font-bold">{title}</h2>
-          <p className="text-sm">{description}</p>
+          <p className="text-sm transition-all group-hover:opacity-20">
+            {description}
+          </p>
+        </div>
+        <div className="flex justify-center gap-4 rounded-b-xl p-2 opacity-0 transition-all duration-300 group-hover:-translate-y-11 group-hover:opacity-100">
+          {link ? (
+            <button
+              className="dui-button dui-button-primary border-primary rounded-xl border bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50 hover:underline"
+              onClick={(): void => {
+                window.location.href = link
+              }}
+            >
+              Visit
+            </button>
+          ) : (
+            <div className="dui-button dui-button-primary border-primary select-none rounded-xl border bg-gray-50 px-2 py-1 text-background-2 opacity-35">
+              Visit
+            </div>
+          )}
+          <button
+            className="dui-button dui-button-primary border-primary group-hover:bg-primary rounded-xl border bg-gray-50 px-2 py-1 text-background-2 hover:bg-interactive-1 hover:text-gray-50"
+            onClick={(): void => {
+              window.location.href = github
+            }}
+          >
+            Github
+          </button>
         </div>
       </div>
     </div>
