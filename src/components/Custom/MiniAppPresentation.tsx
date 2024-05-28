614c5ccb216fd2e8230a84e0db64da5c0cac007f
Tomi-Tom <bariteaupeter.tom@gmail.com>
2024-05-28 21:48:11 +0200

    fix

--- src/components/Custom/MiniAppPresentation.tsx
+++ src/components/Custom/MiniAppPresentation.tsx
@@ -1,1 +1,42 @@
-const MiniAppPresentation = { title, description, link, images }
+import { ReactElement } from 'react'
+
+interface MiniAppPresentationProps {
+  title: string
+  description: string
+  link: string
+  images: string[]
+}
+
+const MiniAppPresentation = ({
+  title,
+  description,
+  link,
+  images,
+}: MiniAppPresentationProps): ReactElement => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center text-4xl font-bold">{title}</h1>
+      <p className="text-center text-lg">{description}</p>
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-500 underline"
+      >
+        {link}
+      </a>
+      <div className="flex flex-wrap justify-center">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={title}
+            className="m-4 h-64 w-64 object-cover"
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default MiniAppPresentation
